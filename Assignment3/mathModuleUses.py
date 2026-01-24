4e9b45865ac2472be80a26f013d0a2c20de50bb2
Ketki0509 <ketkimandavgde@gmail.com>
2026-01-24 18:08:23 +0530

    Submitting Assignment 3

---
+++ Assignment3/mathModuleUses.py
@@ -0,0 +1,10 @@
+import math
+
+num = int(input("Enter a number: "))
+sqrt_of_num = math.sqrt(num)
+log_of_number = math.log(num)
+sine_of_num = math.sin(num)
+
+print(f"Square root: {sqrt_of_num}")
+print(f"Logarithm: {log_of_number}")
+print(f"Sine: {sine_of_num}")
